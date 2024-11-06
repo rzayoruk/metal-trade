e9d024151b2832e20683d78a80da2b0f0338f19e
rzayoruk <rizayoruk180@gmail.com>
2024-11-07 00:08:17 +0300

    getting started with index

--- php/public/index.php
+++ php/public/index.php
@@ -16,11 +16,127 @@ session_start();
     <link href="assets/css/output.css" rel="stylesheet">
 </head>
 
-<body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-    public index.php <br>
+<body class="bg-[#fafafa]">
+    <header class="px-[4%] py-4 bg-[#101b20] text-white">
+        <nav class="flex items-center justify-between">
+            <h1 class="text-2xl"><a href="#">Meta<span class="text-secondary">Lunna</span></a></h1>
+            <ul class="flex gap-x-8">
+                <li class="hover:text-secondary trantisition duration-300"><a href="">Kategoriler</a></li>
+                <li class="hover:text-secondary trantisition duration-300"><a href="">Hakkımızda</a></li>
+                <li class="hover:text-secondary trantisition duration-300"><a href="">Servisler</a></li>
+                <li class="hover:text-secondary trantisition duration-300"><a href="">Kataloglar</a></li>
+                <li class="hover:text-secondary trantisition duration-300"><a href="">İletişim</a></li>
+            </ul>
+            <a href="#" class="py-2.5 px-6 no-highlight border border-white lg:hover:bg-secondary lg:hover:border-secondary transition duration-200">Giriş Yap</a>
+
+
+        </nav>
+    </header>
+
+    <section class="bg-[url('/assets/img/welding_hero2.png')] aspect-[8/3] bg-no-repeat bg-cover">
+
+    </section>
+
+    <section class="px-[4%] pb-20 bg-[url('/assets/img/42tb4et.png')] bg-left bg-no-repeat">
+        <h2 class=" text-3xl font-normal mb-4 pt-16">
+            Çok Satanlar
+        </h2>
+        <div class="grid grid-flow-col grid-cols-5 pt-4 gap-x-10 overflow-x auto-cols-min">
+
+
+
+
+            <div class="flex flex-col  border border-accent3 rounded-md">
+                <img src="/assets/img/nut.png" class="aspect-square mb-2 rounded-t-md" alt="">
+
+                <div class="px-3 pb-3">
+                    <p class="font-semibold mb-1">Metal Parçalar</p>
+                    <p class="text-accent3 mb-2.5 font-normal">Min sipariş Adedi:100</p>
+
+                    <div class="flex justify-between items-center">
+                        <p class="font-semibold">Birim Fiyatı:</p>
+                        <p class="p-2 bg-secondary inline rounded-md text-white">2.40 TL</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col  border border-accent3 rounded-md">
+                <img src="/assets/img/nut.png" class="aspect-square mb-2 rounded-t-md" alt="">
+
+                <div class="px-3 pb-3">
+                    <p class="font-semibold mb-1">Metal Parçalar</p>
+                    <p class="text-accent3 mb-2.5 font-normal">Min sipariş Adedi:100</p>
+
+                    <div class="flex justify-between items-center">
+                        <p class="font-semibold">Birim Fiyatı:</p>
+                        <p class="p-2 bg-secondary inline rounded-md text-white">2.40 TL</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col  border border-accent3 rounded-md">
+                <img src="/assets/img/nut.png" class="aspect-square mb-2 rounded-t-md" alt="">
+
+                <div class="px-3 pb-3">
+                    <p class="font-semibold mb-1">Metal Parçalar</p>
+                    <p class="text-accent3 mb-2.5 font-normal">Min sipariş Adedi:100</p>
+
+                    <div class="flex justify-between items-center">
+                        <p class="font-semibold">Birim Fiyatı:</p>
+                        <p class="p-2 bg-secondary inline rounded-md text-white">2.40 TL</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col  border border-accent3 rounded-md">
+                <img src="/assets/img/nut.png" class="aspect-square mb-2 rounded-t-md" alt="">
+
+                <div class="px-3 pb-3">
+                    <p class="font-semibold mb-1">Metal Parçalar</p>
+                    <p class="text-accent3 mb-2.5 font-normal">Min sipariş Adedi:100</p>
+
+                    <div class="flex justify-between items-center">
+                        <p class="font-semibold">Birim Fiyatı:</p>
+                        <p class="p-2 bg-secondary inline rounded-md text-white">2.40 TL</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col  border border-accent3 rounded-md">
+                <img src="/assets/img/nut.png" class="aspect-square mb-2 rounded-t-md" alt="">
+
+                <div class="px-3 pb-3">
+                    <p class="font-semibold mb-1">Metal Parçalar</p>
+                    <p class="text-accent3 mb-2.5 font-normal">Min sipariş Adedi:100</p>
+
+                    <div class="flex justify-between items-center">
+                        <p class="font-semibold">Birim Fiyatı:</p>
+                        <p class="p-2 bg-secondary inline rounded-md text-white">2.40 TL</p>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    </section>
+
+
+    <section class="pb-16 bg-accent5 h-[800px] relative">
+
+        <!-- bgimage left -->
+        <div class="aspect-[393/455] bg-[url('/assets/img/42tb4et.png')] absolute  bottom-0 bg-no-repeat w-[393px] bg-cover opacity-20">
+        </div>
+        <!-- bgimage right -->
+        <div class="aspect-[393/455] bg-[url('/assets/img/53yj53b.png')] absolute  top-0 right-0 bg-no-repeat w-[393px] bg-cover opacity-20">
+        </div>
+    </section>
+
+    <div class="h-40"></div>
+
 
     <?php if (isset($_SESSION["name"])): ?>
         Welcome <?= $_SESSION["name"] ?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,67 @@
 module.exports = {
   content: ["./**/*.{html,php}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      viet: ["Be Vietnam Pro", "sans-serif"],
+    },
+
+    screens: {
+      ms: "320px",
+      // => @media (min-width: 320px) { ... }
+      msix: "420px",
+      // => @media (min-width: 500px) { ... }
+
+      msi: "506px",
+      // => @media (min-width: 500px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      mdd: "900px",
+
+      mdi: "969px",
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      xli: "1400px",
+      // => @media (min-width: 1400px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+
+      "2xli": "1600px",
+      // => @media (min-width: 1600px) { ... }
+
+      "2xlix": "1700px",
+      // => @media (min-width: 1700px) { ... }
+
+      "2xlie": "1800px",
+      // => @media (min-width: 1800px) { ... }
+
+      "2xliex": "1920px",
+      // => @media (min-width: 1800px) { ... }
+
+      "3xl": "2000px",
+      // => @media (min-width: 2100px) { ... }
+    },
+
+    extend: {
+
+
+      colors: {
+        primary: "#101b20",
+        secondary: "#E72F2A",
+        accent3: "#cbcbcb",
+        accent4: "#FAFAFA",
+        accent5: "#11191f",
+      },
+    },
   },
   plugins: [],
 }
